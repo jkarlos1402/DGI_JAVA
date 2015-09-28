688d1532522714b26ac09160a30530d1cc2009b4
intel core i 7 <intel core i 7@acer>
2015-09-28 11:25:40 -0500

    inicial

---
+++ src/main/java/com/sgi/dao/HibernateUtil.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sgi.dao;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+/**
+ * Hibernate Utility class with a convenient method to get Session Factory
+ * object.
+ *
+ * @author intel core i 7
+ */
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+    
+    static {
+        try {
+            final StandardServiceRegistry registry = new StandardServiceRegistryBuilder().configure().build();
+            sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
+        } catch (Throwable ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+    
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+}
